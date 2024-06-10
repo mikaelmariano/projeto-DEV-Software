6f61ed881118f32884e70216caa2e1d98e75e9db
JoaoLeich <joaopedroleichnioski@gmail.com>
2024-06-10 19:47:13 -0300

    Paciente Controller2

--- clinicamedica/src/br/edu/up/controles/ControlePaciente.java
+++ clinicamedica/src/br/edu/up/controles/ControlePaciente.java
@@ -47,50 +47,5 @@ public class ControlePaciente {
         return pacientesSemCabecalho;
     }
 
-    public static Paciente buscarPacientePorCpf(String cpf) {
-        List<String[]> pacientes = gerenciadorDeArquivos.lerArquivo(CAMINHO_PACIENTES);
-        for (int i = 1; i < pacientes.size(); i++) {  // Pula o cabeçalho
-            String[] paciente = pacientes.get(i);
-            if (paciente[0].equals(cpf)) {
-                return new Paciente(
-                    paciente[0],
-                    paciente[1],
-                    paciente[2],
-                    paciente[3],
-                    paciente[4]
-                );
-            }
-        }
-        return null;
-    }
-
-    public static void atualizarPaciente(Paciente paciente) {
-        List<String[]> pacientes = gerenciadorDeArquivos.lerArquivo(CAMINHO_PACIENTES);
-        for (int i = 1; i < pacientes.size(); i++) {  // Pula o cabeçalho
-            String[] dadosPaciente = pacientes.get(i);
-            if (dadosPaciente[0].equals(paciente.getCpf())) {
-                pacientes.set(i, new String[]{
-                    paciente.getCpf(),
-                    paciente.getSus(),
-                    paciente.getNome(),
-                    paciente.getDtNascimento(),
-                    paciente.getTelefone()
-                });
-                break;
-            }
-        }
-        gerenciadorDeArquivos.atualizarArquivo(CAMINHO_PACIENTES, pacientes);
-    }
-
-    public static void removerPaciente(String cpf) {
-        List<String[]> pacientes = gerenciadorDeArquivos.lerArquivo(CAMINHO_PACIENTES);
-        for (int i = 1; i < pacientes.size(); i++) {  // Pula o cabeçalho
-            String[] dadosPaciente = pacientes.get(i);
-            if (dadosPaciente[0].equals(cpf)) {
-                pacientes.remove(i);
-                break;
-            }
-        }
-        gerenciadorDeArquivos.atualizarArquivo(CAMINHO_PACIENTES, pacientes);
-    }
+    
 }
